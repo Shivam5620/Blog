d6d2ff04900bf865ab013a668c6d8c93263eea56
sivam5620 <sbirla04082000@gmail.com>
2024-05-30 17:52:38 +0530

    update

--- client/src/Redux/actions/articleActions.js
+++ client/src/Redux/actions/articleActions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getArticles = () => async (dispatch) => {
   try {
-    const res = await axios.get('http://localhost:5000/api/articles');
+    const res = await axios.get('https://blog-backend-bgu1.onrender.com/api/articles');
     dispatch({ type: 'GET_ARTICLES', payload: res.data });
   } catch (err) {
     console.error(err);
@@ -11,7 +11,7 @@ export const getArticles = () => async (dispatch) => {
 
 export const createArticle = (article) => async (dispatch) => {
   try {
-    const res = await axios.post('http://localhost:5000/api/articles', article);
+    const res = await axios.post('https://blog-backend-bgu1.onrender.com/api/articles', article);
     dispatch({ type: 'CREATE_ARTICLE', payload: res.data });
   } catch (err) {
     console.error(err);
@@ -20,7 +20,7 @@ export const createArticle = (article) => async (dispatch) => {
 
 export const editArticle = (id, article) => async (dispatch) => {
   try {
-    const res = await axios.put(`http://localhost:5000/api/articles/${id}`, article);
+    const res = await axios.put(`https://blog-backend-bgu1.onrender.com/api/articles/${id}`, article);
     dispatch({ type: 'EDIT_ARTICLE', payload: res.data });
   } catch (err) {
     console.error(err);
@@ -29,7 +29,7 @@ export const editArticle = (id, article) => async (dispatch) => {
 
 export const deleteArticle = (id) => async (dispatch) => {
   try {
-    await axios.delete(`http://localhost:5000/api/articles/${id}`);
+    await axios.delete(`https://blog-backend-bgu1.onrender.com/api/articles/${id}`);
     dispatch({ type: 'DELETE_ARTICLE', payload: id });
   } catch (err) {
     console.error(err);
